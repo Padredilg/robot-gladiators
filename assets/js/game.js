1d16a2ec85700be1b8d8f499e0ad3c9be79d8f92
Luiz P <padredilg@knights.ucf.edu>
2021-07-06 16:23:36 -0400

    randomized atacking order

--- assets/js/game.js
+++ assets/js/game.js
@@ -35,40 +35,50 @@ var randomNumber = function(min, max){
 };
 
 var fight = function(enemy) {
+
+    var isPlayerTurn = true;
+
+    if (Math.random()>.5){
+        isPlayerTurn = false;
+    }
     
     while(enemy.health > 0 && playerInfo.health > 0) {
 
-        if(fightOrSkip()){//breaks only if skip is confirmed in fightOrSkip function
-            break;
-        }
+        if(isPlayerTurn){
+            if(fightOrSkip()){//breaks only if skip is confirmed in fightOrSkip function
+                break;
+            }
 
-        //IF NOT SKIPPED, THEN PLAYER ATTACKS
-        var damage = randomNumber(playerInfo.attack-3, playerInfo.attack);
-        enemy.health = Math.max(0, enemy.health - damage);
-        console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
-        
-        //CHECK ENEMY'S HEALTH STATUS
-        if (enemy.health <= 0) {
-            window.alert(enemy.name + " has died!");
-            break;
-        } 
-        else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            //IF NOT SKIPPED, THEN PLAYER ATTACKS
+            var damage = randomNumber(playerInfo.attack-3, playerInfo.attack);
+            enemy.health = Math.max(0, enemy.health - damage);
+            console.log(playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining.");
+            
+            //CHECK ENEMY'S HEALTH STATUS
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died!");
+                playerInfo.money += 20;
+                break;
+            } 
+            else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
         }
-        
-        //ENEMY ATTACKS
-        var damage = randomNumber(enemy.attack-3, enemy.attack);
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-        console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
-        
-        //CHECK PLAYER HEALTH STATUS
-        if (playerInfo.health <= 0) {
-            window.alert(playerInfo.name + " has died!");
-            break;
-        } 
-        else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+        else{//ENEMY ATTACKS
+            var damage = randomNumber(enemy.attack-3, enemy.attack);
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+            console.log(enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining.");
+            
+            //CHECK PLAYER HEALTH STATUS
+            if (playerInfo.health <= 0) {
+                window.alert(playerInfo.name + " has died!");
+                break;
+            } 
+            else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
         }
+        isPlayerTurn=!isPlayerTurn;//because one turn is for the if, the next is for the else        
     }//closing bracket of while loop
 };
 
